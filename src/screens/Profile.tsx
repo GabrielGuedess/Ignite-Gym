57516f94a0f31f6affe32bd9f96e3c92c673c6d0
Gabriel Guedes <gabrielrguedess@gmail.com>
2023-01-31 11:41:37 -0300

    feat: finished screen component <Profile />

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,79 @@
-import { Center, Text } from "native-base";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
 
-export const Profile = () => (
-  <Center flex={1}>
-    <Text color="white">Profile</Text>
-  </Center>
-);
+import {
+  Center,
+  ScrollView,
+  VStack,
+  Skeleton,
+  Text,
+  Heading,
+} from "native-base";
+
+import { Input } from "components/Input";
+import { Button } from "components/Button";
+import { UserPhoto } from "components/UserPhoto";
+import { ScreenHeader } from "components/ScreenHeader";
+
+const photoSize = 33;
+
+export const Profile = () => {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              w={photoSize}
+              h={photoSize}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              size={photoSize}
+              source={{ uri: "https://github.com/GabrielGuedess.png" }}
+              alt="Foto do usuário"
+            />
+          )}
+
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input placeholder="Nome" bg="gray.600" />
+          <Input placeholder="E-mail" bg="gray.600" isDisabled />
+
+          <Heading
+            alignSelf="flex-start"
+            color="gray.200"
+            fontSize="md"
+            mb={2}
+            mt={12}
+          >
+            Alterar senha
+          </Heading>
+
+          <Input placeholder="Senha antiga" bg="gray.600" secureTextEntry />
+          <Input placeholder="Nova senha" bg="gray.600" secureTextEntry />
+          <Input placeholder="Confirmar senha" bg="gray.600" secureTextEntry />
+
+          <Button title="Atualizar" mt={4} />
+        </Center>
+      </ScrollView>
+    </VStack>
+  );
+};
